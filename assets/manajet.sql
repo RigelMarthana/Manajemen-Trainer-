0e74732db11ab415f2297ba78cbfd3081c674b29
Rigel Iksandy Marthana <seimasu88@gmail.com>
2021-05-28 23:54:14 +0700

    Create manajet.sql

---
+++ assets/manajet.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 28, 2021 at 06:53 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `manajet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_jabatan`
+--
+
+CREATE TABLE `data_jabatan` (
+  `id_jabatan` int(11) NOT NULL,
+  `nama_jabatan` varchar(120) NOT NULL,
+  `gaji_pokok` varchar(50) NOT NULL,
+  `tj_transport` varchar(50) NOT NULL,
+  `uang_makan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_jabatan`
+--
+
+INSERT INTO `data_jabatan` (`id_jabatan`, `nama_jabatan`, `gaji_pokok`, `tj_transport`, `uang_makan`) VALUES
+(1, 'Staff Marketing', '5000000', '1000000', '2000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_kehadiran`
+--
+
+CREATE TABLE `data_kehadiran` (
+  `id_kehadiran` int(11) NOT NULL,
+  `bulan` varchar(15) NOT NULL,
+  `nik` varchar(50) NOT NULL,
+  `nama_trainer` varchar(225) NOT NULL,
+  `jenis_kelamin` varchar(50) NOT NULL,
+  `nama_jabatan` varchar(50) NOT NULL,
+  `hadir` int(11) NOT NULL,
+  `sakit` int(11) NOT NULL,
+  `alpha` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_kehadiran`
+--
+
+INSERT INTO `data_kehadiran` (`id_kehadiran`, `bulan`, `nik`, `nama_trainer`, `jenis_kelamin`, `nama_jabatan`, `hadir`, `sakit`, `alpha`) VALUES
+(2, '052021', '123465789123', 'Erymare', 'perempuan', 'Staff Marketing', 30, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_trainer`
+--
+
+CREATE TABLE `data_trainer` (
+  `id_trainer` int(11) NOT NULL,
+  `nik` varchar(50) NOT NULL,
+  `nama_trainer` varchar(225) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `password` varchar(120) NOT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `photo` varchar(225) NOT NULL,
+  `hak_akses` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_trainer`
+--
+
+INSERT INTO `data_trainer` (`id_trainer`, `nik`, `nama_trainer`, `username`, `password`, `jenis_kelamin`, `jabatan`, `tanggal_masuk`, `status`, `photo`, `hak_akses`) VALUES
+(1, '123465789123', 'Erymare', 'Erymare', 'cea4488334d6a589a50d8d7c8d0f4351', 'perempuan', 'Staff Marketing', '2021-05-01', 'Karyawan Tetap', 'me.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hak_akses`
+--
+
+CREATE TABLE `hak_akses` (
+  `id` int(11) NOT NULL,
+  `keterangan` varchar(50) NOT NULL,
+  `hak_akses` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hak_akses`
+--
+
+INSERT INTO `hak_akses` (`id`, `keterangan`, `hak_akses`) VALUES
+(1, 'admin', 1),
+(2, 'pegawai', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `potongan_gaji`
+--
+
+CREATE TABLE `potongan_gaji` (
+  `id` int(11) NOT NULL,
+  `potongan` varchar(120) NOT NULL,
+  `jml_potongan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `potongan_gaji`
+--
+
+INSERT INTO `potongan_gaji` (`id`, `potongan`, `jml_potongan`) VALUES
+(1, 'Alpha', 500000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_jabatan`
+--
+ALTER TABLE `data_jabatan`
+  ADD PRIMARY KEY (`id_jabatan`);
+
+--
+-- Indexes for table `data_kehadiran`
+--
+ALTER TABLE `data_kehadiran`
+  ADD PRIMARY KEY (`id_kehadiran`);
+
+--
+-- Indexes for table `data_trainer`
+--
+ALTER TABLE `data_trainer`
+  ADD PRIMARY KEY (`id_trainer`);
+
+--
+-- Indexes for table `hak_akses`
+--
+ALTER TABLE `hak_akses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `potongan_gaji`
+--
+ALTER TABLE `potongan_gaji`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_jabatan`
+--
+ALTER TABLE `data_jabatan`
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `data_kehadiran`
+--
+ALTER TABLE `data_kehadiran`
+  MODIFY `id_kehadiran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `data_trainer`
+--
+ALTER TABLE `data_trainer`
+  MODIFY `id_trainer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `hak_akses`
+--
+ALTER TABLE `hak_akses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `potongan_gaji`
+--
+ALTER TABLE `potongan_gaji`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
